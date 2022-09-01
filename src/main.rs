ae5d80ba6a7977a1055701761ffccedfd7d6da74
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2022-09-01 22:13:24 +0530

    template of the app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{self, Parser};
+
+#[derive(Parser, Default, Debug)]
+#[clap(
+    author = "Giri Priyadarshan <github/giripriyadarshan>",
+    version,
+    about = "Rust backend server template generator"
+)]
+struct Arguments {
+    #[clap(forbid_empty_values = true)]
+    /// The name of the server
+    name: String,
+
+    #[clap(short, long)]
+    /// ORM to be used (diesel, sea-orm)
+    /// Default: diesel
+    orm: Option<String>,
+
+    #[clap(short, long)]
+    /// library/framework to be used (actix, warp, axum, tonic)
+    library: String,
+
+    #[clap(short, long)]
+    /// database to be used (postgres, mysql, sqlite)
+    /// default: postgres
+    database: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    println!("{:?}", args);
 }
